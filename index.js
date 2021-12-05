6545d69c4e5ea3e0b13dd75d5afb3fc309f0d3b5
writebooksonline <89612530+mathwebsite@users.noreply.github.com>
2021-12-05 13:42:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function go(){
+  if(document.getElementById("option").value == 1){
+    window.location.hash ="#math";
+    math()
+  }
+}
+function math(){
+  document.getElementById("main").style.display ="none";
+ var options = ["<a href='#math+adding'>adding</a>"]
+ document.write(options);
+  if(window.location.hash =="#math+adding"){
+    document.getElementById("adding").style.display ="block";
+    options = false;
+  }
+  if(window.location.hash == "#math+adding+1+20"){
+    document.getElementById("1plus20").style.display ="block";  
+  }
+}
+if(window.location.hash == "#math"){
+  math()
+}
+if(window.location.hash =="#math+adding"){
+  math()
+}
+ if(window.location.hash == "#math+adding+1+20"){
+    document.getElementById("1plus20").style.display ="block";  
+ }
+if(window.undifiend){
+  window.location.assign("404.html");
+}
