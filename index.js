08930d0ae5b66621f89818645eb910acb142cf87
writebooksonline <89612530+mathwebsite@users.noreply.github.com>
2021-12-05 14:07:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function go(){
   if(document.getElementById("option").value == 1){
-    window.location.href = "https://mathwebsite.github.io?math";
+    window.location.href = "https://mathwebsite.github.io#math";
     math()
   }
 }
@@ -8,21 +8,21 @@ function math(){
   document.getElementById("main").style.display ="none";
  var options = ["<a href='#math+adding'>adding</a>"]
  document.write(options);
-  if(window.location.href =="https://mathwebsite.github.io?math=adding"){
+  if(window.location.href =="https://mathwebsite.github.io#math=adding"){
     document.getElementById("adding").style.display ="block";
     options = false;
   }
-  if(window.location.href== "https://mathwebsite.github.io?math=adding+1+20"){
+  if(window.location.href== "https://mathwebsite.github.io#math=adding+1+20"){
     document.getElementById("1plus20").style.display ="block";  
   }
 }
-if(window.location.href == "https://mathwebsite.github.io?math"){
+if(window.location.href == "https://mathwebsite.github.io#math"){
   math()
 }
-if(window.location.href =="https://mathwebsite.github.io?math"){
+if(window.location.href =="https://mathwebsite.github.io#math"){
   math()
 }
- if(window.location.href == "https://mathwebsite.github.io?math=adding+1+20"){
+ if(window.location.href == "https://mathwebsite.github.io#math=adding+1+20"){
     document.getElementById("1plus20").style.display ="block";  
  }
 if(window.undifiend){
