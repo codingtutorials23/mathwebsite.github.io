9266d954e8385f61acc76b6c39c70c387a0d51bb
writebooksonline <89612530+mathwebsite@users.noreply.github.com>
2021-12-05 13:52:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function go(){
   if(document.getElementById("option").value == 1){
-    window.location.hash = "https://mathwebsite.github.io?math";
+    window.location.href = "https://mathwebsite.github.io?math";
     math()
   }
 }
@@ -22,7 +22,7 @@ if(window.location.href == "https://mathwebsite.github.io?math"){
 if(window.location.href =="https://mathwebsite.github.io?math"){
   math()
 }
- if(window.location.hash == "https://mathwebsite.github.io?math=adding+1+20"){
+ if(window.location.href == "https://mathwebsite.github.io?math=adding+1+20"){
     document.getElementById("1plus20").style.display ="block";  
  }
 if(window.undifiend){
