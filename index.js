65c82784c13b7805ec7372f83762f7bed2561133
writebooksonline <89612530+mathwebsite@users.noreply.github.com>
2021-12-05 14:58:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,20 @@ if(window.location.hash =="#math+adding"){
   document.getElementById("adding").style.display ="block";
   document.getElementById("main").style.display ="none";
 }
+var fs = require("fs");
+console.log("Going to write into existing file");
+// Open a new file with name input.txt and write Simply Easy Learning! to it.
+fs.writeFile('404.java', function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Data written successfully!");
+   console.log("Let's read newly written data");
+   // Read the newly written file and print all of its content on the console
+   fs.readFile('404.java', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      document.write("Asynchronous read: " + data.toString());
+   });
+});
