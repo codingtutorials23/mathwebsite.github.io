7f5b94f051cb19f07cbd6738b685a8f6885a21cd
writebooksonline <89612530+mathwebsite@users.noreply.github.com>
2021-12-05 15:08:42 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <?php
 if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])){
 $refuri = parse_url($_SERVER['HTTP_REFERER']); // use the parse_url() function to create an array containing information about the domain
-if($refuri['host'] == "your-domain.com"){
+if($refuri['host'] == "mathwebsite.github.io"){
 
 //the link was on your site
 }
